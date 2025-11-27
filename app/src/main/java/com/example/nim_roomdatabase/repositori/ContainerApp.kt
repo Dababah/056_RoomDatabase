e721e0ef5719ee417a925a8f5be50fc3c0288cc4
Dababah <pawasalee96@gamil.com>
2025-11-27 14:58:14 +0700

    feat: tambahkan container untuk dependency injection repositori siswa

--- app/src/main/java/com/example/nim_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/nim_roomdatabase/repositori/ContainerApp.kt
@@ -1,9 +1,17 @@
+package com.example.nim_roomdatabase.repositori
 
-class AplikasiSiswa : Application(){
-    lateinit var container : ContainerApp
+import android.app.Application
+import com.example.nim_roomdatabase.room.DatabaseSiswa
 
-    override fun onCreate() {
-        super.onCreate()
-        container = ContainerDataApp(this)
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
     }
 }
+
