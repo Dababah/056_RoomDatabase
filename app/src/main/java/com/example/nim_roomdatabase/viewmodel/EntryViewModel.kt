5fb74f71778f74908e3d6d82999290debc406bb2
Dababah <pawasalee96@gamil.com>
2025-11-27 15:28:55 +0700

    definisikan UIStateSiswa dan DetailSiswa sebagai representasi state form

---
+++ app/src/main/java/com/example/nim_roomdatabase/viewmodel/EntryViewModel.kt
@@ -0,0 +1,18 @@
+
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telepon: String = "",
+)
+
+/**
+ * Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya
+ */
+
+
