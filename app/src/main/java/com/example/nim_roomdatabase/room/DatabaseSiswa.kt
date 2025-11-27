0b53b67480ffe0ba4d31cdb8a10b80e0ac9d8d48
Dababah <pawasalee96@gamil.com>
2025-11-27 15:03:12 +0700

    buat entitas dan DAO untuk data siswa

--- app/src/main/java/com/example/nim_roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/nim_roomdatabase/room/DatabaseSiswa.kt
@@ -1,4 +1,11 @@
 package com.example.nim_roomdatabase.room
 
-class DatabaseSiswa {
-}
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1, exportSchema = false)
+abstract class DatabaseSiswa : RoomDatabase() {
+
+    abstract fun siswaDao(): SiswaDao
