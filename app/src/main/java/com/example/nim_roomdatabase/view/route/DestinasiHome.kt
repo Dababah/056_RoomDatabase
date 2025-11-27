399b291f1350fb59089e80085f04a2bbe8df3838
Dababah <pawasalee96@gamil.com>
2025-11-27 15:14:09 +0700

    tambahkan destinasi rute halaman utama

---
+++ app/src/main/java/com/example/nim_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.nim_roomdatabase.view.route
+
+import com.example.nim_roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
