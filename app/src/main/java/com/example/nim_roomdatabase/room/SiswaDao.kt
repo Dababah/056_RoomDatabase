c73cb2faa74cf6771d7c072f818750c210ce5765
Dababah <pawasalee96@gamil.com>
2025-11-27 15:11:51 +0700

    tambahkan query untuk ambil semua data siswa terurut

---
+++ app/src/main/java/com/example/nim_roomdatabase/room/SiswaDao.kt
@@ -0,0 +1,8 @@
+package com.example.nim_roomdatabase.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
