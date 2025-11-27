405d30c197aa53b428c56a6cad639643b5fdeda6
Dababah <pawasalee96@gamil.com>
2025-11-27 15:05:00 +0700

    amankan inisialisasi database dengan synchronized

--- app/src/main/java/com/example/nim_roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/nim_roomdatabase/room/DatabaseSiswa.kt
@@ -1,13 +1,16 @@
-package com.example.nim_roomdatabase.room
-
-import android.content.Context
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
 
 
-abstract class DatabaseSiswa : RoomDatabase() {
+    companion object {
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
 
-    abstract fun siswaDao(): SiswaDao
-
-    
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(
+                    context, DatabaseSiswa::class.java,
+                    "siswa_database")
+                    .build().also { Instance = it }
+            })
+        }
+    }
+}
