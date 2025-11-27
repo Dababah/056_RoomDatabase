b1a401dc2dd1aeb4a37bf245cb76e54d184caf83
Dababah <pawasalee96@gamil.com>
2025-11-27 14:59:35 +0700

    gunakan lazy initialization pada repositori siswa via Room DAO

--- app/src/main/java/com/example/nim_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/nim_roomdatabase/repositori/ContainerApp.kt
@@ -3,15 +3,16 @@ package com.example.nim_roomdatabase.repositori
 import android.app.Application
 import com.example.nim_roomdatabase.room.DatabaseSiswa
 
-interface ContainerApp {
-    val repositoriSiswa : RepositoriSiswa
-}
 
-class ContainerDataApp(private val context: Context):
-    ContainerApp {
-    override val repositoriSiswa: RepositoriSiswa by lazy {
-        OfflineRepositoriSiswa(
-            DatabaseSiswa.getDatabase(context).siswaDao())
+class AplikasiSiswa : Application() {
+    /**
+     * AppContainer instance digunakan oleh kelas-kelas lainnya
+     * untuk mendapatkan dependensi
+     */
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
     }
 }
-
