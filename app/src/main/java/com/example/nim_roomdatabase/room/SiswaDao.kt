04ae54c84b4f46c263b2f47987ecd3b53cf8f6e6
Dababah <pawasalee96@gamil.com>
2025-11-27 15:12:26 +0700

    tambahkan operasi insert dengan penanganan konflik

--- app/src/main/java/com/example/nim_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/nim_roomdatabase/room/SiswaDao.kt
@@ -1,8 +1,4 @@
-package com.example.nim_roomdatabase.room
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
-
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+}
