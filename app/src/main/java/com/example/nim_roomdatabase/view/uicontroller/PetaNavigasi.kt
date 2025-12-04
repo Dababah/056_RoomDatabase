afdfe7a157c5b323996c8883abb061981ae360c3
Dababah <pawasalee96@gamil.com>
2025-12-04 12:43:15 +0700

    Perbaiki route detail siswa dengan argumen ID dan gunakan navigateUp

--- app/src/main/java/com/example/nim_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/nim_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -4,35 +4,45 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.nim_roomdatabase.view.EntrySiswaScreen
 import com.example.nim_roomdatabase.view.HomeScreen
+import com.example.nim_roomdatabase.view.route.DestinasiDetailSiswa
 import com.example.nim_roomdatabase.view.route.DestinasiEntry
 import com.example.nim_roomdatabase.view.route.DestinasiHome
+import com.example.nim_roomdatabase.view.route.DetailSiswaScreen
 
 @Composable
-fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier){
-    HostNavigasi(navController = navController)
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun HostNavigasi(
-    navController: NavHostController,
+fun SiswaApp(
+    navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
-){
-    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
-    {
-        composable(DestinasiHome.route){
-            HomeScreen(
-                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
-            )
-        }
-        composable(DestinasiEntry.route){
-            EntrySiswaScreen(navigateBack = { navController.popBackStack()})
-        }
+) {
 
+
+
+        composable(
+            route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetailSiswa.itemIdArg) { // Hapus titik (.) sebelum navArgument
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            DetailSiswaScreen(
+                navigateBack = { navController.navigateUp() })
+        }
+        composable (route=DestinasiEditSiswa.routeWithArgs,
+            argument= listOf(navArgument(DestinasiEditSiswa.itemIdArg){
+                type= NavType.IntType })){
+            EditSiswaScreen(
+                navigateback = {navController.popBackStack()},
+                onnavigateup = {navController.navigateUp()}
+            )
+
+            }
     }
 }
