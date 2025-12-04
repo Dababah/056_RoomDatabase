c744f77923e8bdf1d509f6582c56c92bffd1f23f
Dababah <pawasalee96@gamil.com>
2025-12-04 14:26:31 +0700

    tambahin EditViewModel dan sambungin ke factory

--- app/src/main/java/com/example/nim_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/nim_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,34 +1,5 @@
-package com.example.nim_roomdatabase.viewmodel.provider
 
 
-
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.createSavedStateHandle
-import androidx.lifecycle.viewmodel.CreationExtras
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.nim_roomdatabase.repositori.AplikasiSiswa
-import com.example.nim_roomdatabase.viewmodel.DetailViewModel
-import com.example.nim_roomdatabase.viewmodel.EntryViewModel
-import com.example.nim_roomdatabase.viewmodel.HomeViewModel
-
-
-
-    val Factory = viewModelFactory{
-        initializer {
-            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
-        }
-
-        initializer {
-            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
-        }
-
-        initializer {
-            DetailViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
-        }
-        initializer{
-            EditViewModel(this.createSavedStateHandle(),aplikasiSiswa().container.repositoriSiswa)
-        }
-    }
-}
-
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory
+        .APPLICATION_KEY] as AplikasiSiswa)
