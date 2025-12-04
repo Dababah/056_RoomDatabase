2aec44f0d47e12799dcf5fd941d8665978bde0fb
Dababah <pawasalee96@gamil.com>
2025-12-04 14:23:17 +0700

    feat(route): tambahkan DestinasiEditSiswa dengan argumen ID untuk navigasi edit

---
+++ app/src/main/java/com/example/nim_roomdatabase/view/route/DestinasiEditSiswa.kt
@@ -0,0 +1,8 @@
+package com.example.nim_roomdatabase.view.route
+import com.example.nim_roomdatabase.R
+
+object DestinasiEditSiswa : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
