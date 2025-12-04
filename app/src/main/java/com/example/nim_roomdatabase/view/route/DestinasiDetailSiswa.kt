7b94c8ed581253705fe8c88c6139c4da8fbbd79f
Dababah <pawasalee96@gamil.com>
2025-12-04 12:38:00 +0700

    Tambahkan destinasi detail siswa dengan argumen ID

---
+++ app/src/main/java/com/example/nim_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.nim_roomdatabase.view.route
+
+import com.example.nim_roomdatabase.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
