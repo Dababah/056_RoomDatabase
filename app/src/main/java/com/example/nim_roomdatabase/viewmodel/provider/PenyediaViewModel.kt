ca11d8158471f08ee9b8be2b9693ae21605b25fc
Dababah <pawasalee96@gamil.com>
2025-12-04 14:25:18 +0700

    siapin viewModelFactory dasar + Home & Entry

---
+++ app/src/main/java/com/example/nim_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.nim_roomdatabase.viewmodel.provider
+
+
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.nim_roomdatabase.repositori.AplikasiSiswa
+import com.example.nim_roomdatabase.viewmodel.DetailViewModel
+import com.example.nim_roomdatabase.viewmodel.EntryViewModel
+import com.example.nim_roomdatabase.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+
